abb2c00b981c2d6f5518cce307bd50afb7fd61f3
Pedroprog2 <45775554+Pedroprog2@users.noreply.github.com>
2024-07-13 17:50:32 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -157,7 +157,7 @@ if uploaded_file is not None:
 # Exibir o gráfico
     #plt.show()
 
-    plt.hist(mvetor_concatenado[,:].T, bins=768, histtype='step', fill=True, color='skyblue')
+    plt.hist(mvetor_concatenado[:,:].T, bins=768, histtype='step', fill=True, color='skyblue')
     plt.show()
 
     #print(vetor_concatenado.shape)
